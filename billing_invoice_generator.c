bca3ebbfdc124de52e2c673076d192527f0b9b7e
elnatty <alabinathan1916@gmail.com>
2022-06-06 07:37:06 +0100

    main file

---
+++ billing_invoice_generator.c
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdio.h>
+
+// billgenerator fuction
+void generateBillHeader (char name[50], char date[30]){
+  printf("\n\n");
+    printf("\t    DKING. Invoice Gen");
+    
+}
