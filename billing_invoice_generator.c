eb0e354fadbecdcc28bfe84fd7bdb835027ddc82
elnatty <alabinathan1916@gmail.com>
2022-06-06 08:19:53 +0100

    main functions

--- billing_invoice_generator.c
+++ billing_invoice_generator.c
@@ -7,5 +7,30 @@
 void generateBillHeader (char name[50], char date[30]){
   printf("\n\n");
     printf("\t    DKING. Invoice Gen");
-    
+    printf("\n\t  -------------------");
+    printf("\n");
+    printf("\nDate: %s", date);
+    printf("\n");
+    printf("------------------------------------------");
+    printf("Items\t\t");
+    printf("Qty\t\t");
+    printf("Total\t\t");
+    printf("--------------------------------------------");
+    printf("\n\n");   
+}
+// function for billBody
+void generateBillBody (char item[30], int qty, float price){
+  printf("%s\t\t", item);
+    printf("%d\t\t", qty);
+    printf("%.2f\t\t", qty * price);
+    printf("\n");
+}
+// function to generate billfooter
+void generateBillFooter (float total){
+  printf("\n");
+  float dis = 0.1 * total;
+  float netTotal = total - dis;
+  float cgst=0.09*netTotal, granfTotal=netTotal + 2*cgst;
+  printf("---------------------------------------\n");
+  
 }
