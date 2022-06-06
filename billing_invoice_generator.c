eb08afb82639ccda2664afc115f63df05bbff93b
dking <alabinathan1916@gmail.com>
2022-06-06 10:30:23 +0000

    new changes added

--- billing_invoice_generator.c
+++ billing_invoice_generator.c
@@ -1,4 +1,4 @@
-#include<conio.h>
+//#include<conio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<stdio.h>
@@ -25,6 +25,20 @@ void generateBillBody (char item[30], int qty, float price){
     printf("%.2f\t\t", qty * price);
     printf("\n");
 }
+
+// structure of user purchase details.
+struct items{
+  char item[20];
+  float price;
+  int qty;
+};
+struct orders{
+  char customer[50];
+  char date[50];
+  int numOfItems;
+  struct items itm[50];
+};
+
 // function to generate billfooter
 void generateBillFooter (float total){
   printf("\n");
@@ -32,5 +46,55 @@ void generateBillFooter (float total){
   float netTotal = total - dis;
   float cgst=0.09*netTotal, granfTotal=netTotal + 2*cgst;
   printf("---------------------------------------\n");
-  
+  printf("SubTotal\t\t\t%.2f", total);
+  printf("\nDiscount @10%s\t\t\t%.2f", "%", dis);
+  printf("\n\t\t\t\t-------");
+  printf("\nNet Total\t\t\t%.2f", netTotal);
+  printf("\nCGST @9%s\t\t\t%.2f", "%", cgst);
+  printf("\nSGST @9%s\t\t\t%.2f", "%", cgst);
+  printf("\n---------------------------------------");
+  printf("\nGrand Total\t\t\t%.2f", granfTotal);
+  printf("\n--------------------------------------\n");
+}
+
+int main(){
+  // dashboard
+  int opt, n;    // variable to store user preferred operation from line 54.
+  struct orders ord;
+  printf("\n\n================== DKING. Invoice Gen ==================\n");
+  printf("\nplease select your preferred operation:\t");
+  printf("\n\n1. Generate Invoice");
+  printf("\n2. Show all Invoice");
+  printf("\n3. Search Invoice");
+  printf("\n4. Exit");
+  printf("Your choice:\t");
+  scanf("%d", &opt);
+  fgetc(stdin);   // to remove the '\n' created by default by the scanf(line 69).
+
+  switch (opt){
+    case 1:
+      printf("\nPlease enter name of customer:\t");
+      fgets(ord.customer, 50, stdin); //stores cus name, so first we initialize the struct order(line 63), then call the ord.customer to store the cus name.
+      ord.customer[strlen(ord.customer)-1]=0; // to remove the "\n" fgets creates by default.
+      strcpy(ord.date, __DATE__);
+      printf("\nplease enter number of item:\t");
+      scanf("%d", &n);
+      // for loop to take all "n" item.
+      for (int i=0; i<n; i++){
+        fgetc(stdin);
+        printf("\n\n");
+        printf("please enter the item %d:\t", i+1);
+        fgets(ord.itm[i].item, 20, stdin);
+        ord.itm[i].item[strlen(ord.itm[i].item)-1]=0;
+        printf("please enter quantity:\t\t");   // storing qty into ord.itm[i]
+        scanf("%d", &ord.itm[i].qty);
+        printf("please enter the unit price:\t");
+        scanf("%f", &ord.itm[i].price);
+
+      }
+
+  }
+
+  printf("\n\n");
+
 }
